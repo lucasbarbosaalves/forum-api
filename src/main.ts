9bffa3698d220dff286eb63286f6bf447d6002e9
lucasb <lkab05@hotmail.com>
2025-08-20 15:01:46 -0300

    feat: added zod validation on env variables

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'dotenv/config';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService: ConfigService<Env> = app.get(ConfigService);
+  const port = configService.get('PORT', { infer: true }) || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
