21735ac0e2b8b32d968837fe62555fed0aad8bd8
lucasb <lkab05@hotmail.com>
2025-08-20 15:02:13 -0300

    feat: created a global zod-validation-pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-import { CreateAccountController } from './controllers/create-account/create-account.controller';
+import { CreateAccountController } from './controllers/create-account.controller';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './env/env';
 
