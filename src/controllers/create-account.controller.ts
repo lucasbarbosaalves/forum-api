47e01339f98f0de29d460cc183bfd5ea6f02be06
lucasb <lkab05@hotmail.com>
2025-08-20 15:02:44 -0300

    feat: create account controller with zod validation schema and hashed password with bcryptjs

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,45 @@
+import { ZodValidationPipe } from '@/pipe/zod-validation-pipe';
+import { PrismaService } from '@/prisma/prisma.service';
+import { ConflictException, Get, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { hash } from 'bcryptjs';
+import z from 'zod';
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.email(),
+  password: z.string(),
+});
+
+type CreateAccountBody = z.infer<typeof createAccountBodySchema>;
+
+@Controller('accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBody) {
+    const { name, email, password } = body;
+
+    const emailVerification = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (emailVerification) {
+      throw new ConflictException('User with same e-mail address already exists');
+    }
+
+    const hash_password = await hash(password, 8);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hash_password,
+      },
+    });
+  }
+}
