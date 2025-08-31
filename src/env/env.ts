9be9a677ac3b14fbb9a9fa5d7ad6f2e84f4132c9
lucasb <lkab05@hotmail.com>
2025-08-31 19:08:15 -0300

    updated env validation schema

--- src/env/env.ts
+++ src/env/env.ts
@@ -2,9 +2,13 @@ import z from 'zod';
 
 export const envSchema = z.object({
   DATABASE_URL: z.url(),
-  PORT: z.coerce.number().optional().default(3000),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  AWS_BUCKET_NAME: z.string(),
+  AWS_SECRET_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  PORT: z.coerce.number().optional().default(3000),
 });
 
 export type Env = z.infer<typeof envSchema>;
