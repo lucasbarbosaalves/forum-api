df1c3668e4c856e3a9713e7a24f45f6d931c91b4
lucasb <lkab05@hotmail.com>
2025-08-31 19:10:45 -0300

    feat: create storage module with Cloudflare R2 implementation (using s3 client)

---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,55 @@
+import { InvalidAttachmentTypeError } from '@/domain/forum/application/use-cases/errors/invalid-attachment-type-error';
+import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachments';
+import {
+  BadRequestException,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('/attachments')
+export class UploadAttachmentController {
+  constructor(private uploadAndCreateAttachmetn: UploadAndCreateAttachmentUseCase) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }), // 2MB
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg|pdf)' }),
+        ],
+      })
+    )
+    file: Express.Multer.File
+  ) {
+    const result = await this.uploadAndCreateAttachmetn.execute({
+      fileName: file.originalname,
+      fileType: file.mimetype,
+      body: file.buffer,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case InvalidAttachmentTypeError:
+          throw new BadRequestException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+
+    const { attachment } = result.value;
+
+    return {
+      attachmentId: attachment.id.toString(),
+    };
+  }
+}
